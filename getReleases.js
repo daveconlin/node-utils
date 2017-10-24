d808c574606893b69b49adecd430ca2f5766ac15
Dave Conlin <daveconlin@mac.com>
2017-10-24 08:11:30 -0400

    changed to promises

--- getReleases.js
+++ getReleases.js
@@ -1,5 +1,12 @@
+import fetch from "isomorphic-fetch";
+
 const environments = {
-  "dev-o": ["app.min.css", "print.min.css", "app.min.js", "libs.min.js"],
+  "dev-o": [
+    "https://www.regions.com/RDCResources/Content/com-regions-dotcom.min.css",
+    "https://www.regions.com/RDCResources/Content/com-regions-dotcom-print.min.css",
+    "https://www.regions.com/RDCResources/Scripts/com-regions-dotcom-application.min.js",
+    "https://www.regions.com/RDCResources/Scripts/com-regions-dotcom-libs.min.js"
+  ],
   "dev-a": ["app.min.css", "print.min.css", "app.min.js", "libs.min.js"],
   "dev-qa": ["app.min.css", "print.min.css", "app.min.js", "libs.min.js"],
   "prod-a": ["app.min.css", "print.min.css", "app.min.js", "libs.min.js"],
@@ -10,14 +17,15 @@ function fetchFile() {}
 function checkVer() {}
 
 function getReleases(env) {
-  // get environments and loop
-  environments[env].forEach(file => {
-    console.log(env, file);
-    return;
+  return Promise.all(
+    environments[env].map(url => {
+      console.log(url);
+      fetch(url).then(resp => resp.text());
+    })
+  ).then(texts => {
+    texts;
   });
   // pass files to parser to check version
-
-  // return Promises.all();
 }
 
 const results = getReleases("dev-o");
